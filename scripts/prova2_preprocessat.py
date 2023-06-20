621802e3e2efd37dcbdb6a0680edffc3a56ec453
Julia <julia.leon.hermida@gmail.com>
2023-06-20 19:18:47 +0200

    model entrenat i cross validation

--- scripts/prova2_preprocessat.py
+++ scripts/prova2_preprocessat.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OrdinalEncoder, Normalizer
-from skforecast import ForecasterAutoreg
-from xgboost import XGBRegressor
-from sklearn.metrics import accuracy_score
+from xgboost import XGBRegressor, cv, callback, DMatrix
+from sklearn.metrics import mean_squared_error
 
 from processat_df import var_list, train_df, test_df
 
@@ -15,16 +14,18 @@ file='datasets/train_df.csv'
 def crear_dies_previs(data, var):
     var_previ1 = var + '_previ1'
     var_previ2 = var + '_previ2'
-    data[var_previ1] = data.sort_values(by=['data'], axis=0)[var].shift(periods=-1)
-    data[var_previ1].fillna(method='ffill', inplace=True)
-    data[var_previ2] = data.sort_values(by=['data'], axis=0)[var_previ1].shift(periods=-1)
-    data[var_previ2].fillna(method='ffill', inplace=True)
+    data = data.sort_values(by=['data'], axis=0, ignore_index=True)
+    data[var_previ1] = data[var].shift(periods=-1)
+    data[var_previ1] = data[var_previ1].fillna(method='ffill')
+    data[var_previ2] = data[var_previ1].shift(periods=-1)
+    data[var_previ2] = data[var_previ2].fillna(method='ffill')
     return data
 
 def loop_dies_previs(data, var_list):
     df_model = pd.DataFrame()
     for estacio in set(data['nom_estacio']):
-        estacio_df = data[data['nom_estacio']==estacio]
+        estacio_df = data.loc[data['nom_estacio'] == estacio]
+        #estacio_df = data[data['nom_estacio']==estacio]
         for var in var_list:
             estacio_df = crear_dies_previs(data=estacio_df, var=var)            
         df_model = pd.concat([df_model, estacio_df], ignore_index=True)
@@ -34,14 +35,6 @@ def split_target(data):
     y = data['NO_dia']
     x = data.drop('NO_dia', axis=1)
     return x, y
-
-'''
-def get_dummies(data):
-    enc = OrdinalEncoder()
-    cat = data.select_dtypes(include=object)
-    if data==train_df:
-        enc.fit(cat)
-    return pd.DataFrame(enc.transform(cat))'''
 
 def fit_transform_dummies(data):
     enc = OrdinalEncoder()
@@ -53,14 +46,6 @@ def transform_dummies(data, encoder):
     enc = encoder
     cat = data.select_dtypes(include=object)
     return pd.DataFrame(enc.transform(cat))
-
-'''
-def scale_num(data):
-    num = data.select_dtypes(include=np.number)
-    if data==train_df:
-        scaler = Normalizer().fit(num)
-    scaled = scaler.transform(num)
-    return pd.DataFrame(scaled) '''
 
 def fit_transform_num(data):
     num = data.select_dtypes(include=np.number)
@@ -74,15 +59,24 @@ def transform_num(data, scaler):
     scaled = scaler.transform(num)
     return pd.DataFrame(scaled) 
 
+def cross_validation(xtrain, ytrain):
+    param = {'max_depth':2, 'eta':1, 'objective':'reg:squarederror'}
+    num_round = 2
+    xgtrain = DMatrix(xtrain.values, ytrain.values)
+    print('abans de cv')
+    cv_ = cv(params=param, dtrain=xgtrain, num_boost_round=num_round, nfold=5, seed=0)
+    print('després de cv')
+    return cv_
 
 def pipeline_processat_train(data):
     data = loop_dies_previs(data=data, var_list=var_list)
-    x, y = split_target(data=data)
+    x, y_train = split_target(data=data)
     cat, enc = fit_transform_dummies(data=x)
     num, scaler = fit_transform_num(data=x)
     X = pd.concat([data.data, cat, num], axis=1, ignore_index=True)
     x_train = X.set_index([0], drop=True)
-    return x_train, y, enc, scaler
+    cv_ = cross_validation(xtrain=x_train, ytrain=y_train)
+    return x_train, y_train, enc, scaler, cv_
 
 def pipeline_processat_test(data, encoder, scaler):
     data = loop_dies_previs(data=data, var_list=var_list)
@@ -92,23 +86,23 @@ def pipeline_processat_test(data, encoder, scaler):
     X = pd.concat([data.data, cat, num], axis=1, ignore_index=True)
     return X.set_index([0], drop=True), y
 
-#
 
 def model_training(train, test):
-    x_train, y_train, encoder, scaler = pipeline_processat_train(data=train)
+    x_train, y_train, encoder, scaler, cv_ = pipeline_processat_train(data=train)
     forecast = XGBRegressor() #       forecast = ForecasterAutoreg(regressor = XGBRegressor())
     forecast.fit(x_train, y_train)
     x_test, y_test = pipeline_processat_test(data=test, encoder=encoder, scaler=scaler)
     y_pred = forecast.predict(x_test)
-    #accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)#, normalize=False
-    return y_pred, y_test#, accuracy
+    mse = mean_squared_error(y_true=y_test, y_pred=y_pred, squared=False)
+    return y_pred, y_test, mse, cv_
 
 
 
 if __name__ == '__main__':
-    y_pred, y_test = model_training(train=train_df, test=test_df) #, accuracy
-    #print("accuracy: {}".format(accuracy))
-    accuracy_score(y_true=y_test, y_pred=y_pred)
+    y_pred, y_test, mse, cv_ = model_training(train=train_df, test=test_df) 
+    print(var_list)
+    print("mse: {}".format(mse))
+    print("cv: {}".format(cv_))
     print("y_pred:")
     print(y_pred)
     print("y_test:")
